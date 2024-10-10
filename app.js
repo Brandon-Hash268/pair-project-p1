ce93a7824999152a121c4e5bf5eb2084f882b793
Brandon Vincentius <brandonvincentius54321@gmail.com>
2024-10-10 09:37:09 +0700

    migrate

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
-app.
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
